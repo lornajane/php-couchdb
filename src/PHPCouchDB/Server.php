36c8665e0a847771e5826f7eff7ffa5d4d001661
Lorna Jane Mitchell <lorna@lornajane.net>
2018-04-20 15:09:19 +0100

    Mostly formatting fixes

--- src/PHPCouchDB/Server.php
+++ src/PHPCouchDB/Server.php
@@ -170,10 +170,9 @@ class Server
     /**
      * Create a database user and return the revision of the user record (for later updating)
      *
-     * @author David Baltusavich <david@synatree.com>
      * @param  $username the new username
      * @param  $password the password to setup for the user
-     * @param  optional                                    $roles if you want to specify the roles on the server for the user. defaults to []
+     * @param  $roles if you want to specify the roles on the server for the user. defaults to [] (optional)
      * @return string revision string
      * @throws \PHPCouchDB\Exception\ServerException if there's a problem
      */
@@ -189,31 +188,26 @@ class Server
             $response = $this->client->request("PUT", "/_users/org.couchdb.user:" . $username, ['json' => $doc]);
             if ($response->getStatusCode() == 201 && $response_data = json_decode($response->getBody(), true)) {
                 return $response_data['_rev'];
-            }
-            else
-            {
+            } else {
                 throw new Exception\ServerException(
                     'Problem creating user'
                 );
             }
-        } 
-        catch (\GuzzleHttp\Exception\ClientException $e){
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
             throw new Exception\ServerException(
                 'Connection or other Error:' . $e->getMessage()
             );
         }
-        
     }
 
     /**
      * Update a database user and return the revision of the user record (for further updating)
      *
-     * @author David Baltusavich <david@synatree.com>
      * @param  $username the new username
      * @param  $password the new password
-     * @param  optional                  $rev   the revision of the current record (saves a query if you can specify this)
-     * @param  optional                  $roles if you want to specify the roles on the server for the user. defaults to []
-     * @return string revision string
+     * @param  $rev      the revision of the current record (saves a query if you can specify this) (optional)
+     * @param  $roles    if you want to specify the roles on the server for the user. defaults to [] (optional)
+     * @return string    revision string
      * @throws \PHPCouchDB\Exception\ServerException if there's a problem
      */
     public function updateUser($username, $password, $rev = false, $roles = [])
@@ -224,52 +218,43 @@ class Server
             'name' => $username,
             'roles' => $roles,
         ];
-        if(!$rev) {
-            try
-            {
+        if (!$rev) {
+            try {
                 $response = $this->client->request("GET", "/_users/org.couchdb.user:" . $username);
                 if ($response->getStatusCode() == 200 && $response_data = json_decode($response->getBody(), true)) {
                     $rev = $response_data['_rev'];
-                }
-                else
-                {
-                    if($response->getStatusCode() == 404) {
+                } else {
+                    if ($response->getStatusCode() == 404) {
                         throw new Exception\ServerException(
                             "Your connection doesn't have privileges to confirm the user, or the user does not exist"
                         );
                     }
                     throw new Exception\ServerException("Something went wrong: " . $response->getStatusCode());
                 }
-            } 
-            catch (\GuzzleHttp\Exception\ClientException $e){
+            } catch (\GuzzleHttp\Exception\ClientException $e) {
                 throw new Exception\ServerException(
                     'Could not retrieve the username provided'
                 );
             }
         }
-        try
-        {
+        try {
             $response = $this->client->request(
-                "PUT", "/_users/org.couchdb.user:" . $username, [
-                'json' => $doc,
-                'headers' => [
-                    'If-Match' => $rev
-                ],
+                "PUT",
+                "/_users/org.couchdb.user:" . $username,
+                [
+                    'json' => $doc,
+                    'headers' => ['If-Match' => $rev],
                 ]
             );
             if ($response->getStatusCode() == 201 && $response_data = json_decode($response->getBody(), true)) {
                 return $response_data['_rev'];
-            }
-            else
-            {
-                if($response->getStatusCode() == 409) {
+            } else {
+                if ($response->getStatusCode() == 409) {
                     throw new Exception\ServerException("Bad permissions");
                 }
                 throw new Exception\ServerException("Something went wrong: " . $response->getStatusCode());
             }
-        } 
-        catch (\GuzzleHttp\Exception\ClientException $e)
-        {
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
             throw new Exception\ServerException(
                 "Failing Updating User: " . $e->getMessage() . "REV: $rev"
             );
